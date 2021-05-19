64bfd5ce729643879ec2bfefa1da47d4caf37e48
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-05-19 20:19:38 +0700

    Change completed task from strikethrough to green background

--- src/components/TodoLists.jsx
+++ src/components/TodoLists.jsx
@@ -1,35 +1,31 @@
-import { List, ListItem, Checkbox } from '@chakra-ui/react';
+import { Box, List, ListItem, Checkbox } from '@chakra-ui/react';
 
 const TodoLists = ({ todoItems, handleTaskDone }) => {
   return (
     <>
       <List spacing={3} w="100%">
         {todoItems.map(todo => {
+          // Pengen dibuat kalau checklist nanti box list nya warna hijau
           return (
-            <ListItem
-              key={todo.id}
-              d="flex"
-              flexDir="row"
-              alignItems="center"
-              p={3}
-              borderRadius="5px"
-              backgroundColor="gray.100"
-              color="blackAlpha.800"
-            >
-              <Checkbox
-                mr={5}
-                borderColor="gray.500"
-                onChange={e => handleTaskDone(todo.id)}
-              ></Checkbox>
-
-              {todo.completed ? (
-                <span style={{ textDecoration: 'line-through' }}>
-                  {' '}
-                  {todo.title}{' '}
-                </span>
-              ) : (
+            <ListItem key={todo.id}>
+              <Box
+                d="flex"
+                flexDir="row"
+                alignItems="center"
+                p={3}
+                borderRadius="5px"
+                backgroundColor={
+                  todo.completed ? 'green.100' : 'blackAlpha.100'
+                }
+                color="blackAlpha.800"
+              >
+                <Checkbox
+                  mr={5}
+                  borderColor="gray.500"
+                  onChange={e => handleTaskDone(todo.id)}
+                ></Checkbox>
                 <span>{todo.title}</span>
-              )}
+              </Box>
             </ListItem>
           );
         })}
